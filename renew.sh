8137b3c85261981018ba71d32ecbb6a6c539deb6
Ryan Anderson <ryan@joshandryan.net>
2023-07-17 22:20:31 -0500

    cleanup

--- renew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/bin/certbot renew -q --authenticator dns --dns-credentials /root/gandi.ini
-
-openssl pkcs12 -export -out /store/PMS/letsencrypt.pfx -passout pass:plexmediaserver -inkey /etc/letsencrypt/live/plex.joshandryan.net/privkey.pem -in /etc/letsencrypt/live/plex.joshandryan.net/cert.pem -certfile /etc/letsencrypt/live/plex.joshandryan.net/chain.pem
-
-systemctl restart plexmediaserver
-systemctl restart apache2

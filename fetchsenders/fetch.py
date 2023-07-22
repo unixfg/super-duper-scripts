9fb36f7ea29732ba76f958b655f436083f1edc8f
Ryan Anderson <ryan@joshandryan.net>
2023-07-22 00:52:57 -0500

    Address Migration Tool

---
+++ fetchsenders/fetch.py
@@ -0,0 +1,50 @@
+import os
+import imaplib
+import email
+import configparser
+from email.header import decode_header
+from tqdm import tqdm
+
+# read the configuration file
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini'))
+
+# retrieve configuration
+username = config.get('credentials', 'username')
+password = config.get('credentials', 'password')
+server = config.get('credentials', 'server')
+
+# establish a connection
+mail = imaplib.IMAP4_SSL(server)
+# authenticate
+mail.login(username, password)
+
+# select the mailbox
+mail.select("inbox")
+
+# get uids
+result, data = mail.uid('search', None, "ALL")
+# list of uids
+uid_list = data[0].split()
+
+senders = []
+
+# iterate over each email with a progress bar
+for uid in tqdm(uid_list, desc='Processing emails', unit='email'):
+    # fetch headers
+    result, data = mail.uid('fetch', uid, '(BODY[HEADER.FIELDS (FROM)])')
+    raw_email = data[0][1].decode("utf-8")
+    email_message = email.message_from_string(raw_email)
+    # decode the email address
+    from_header = decode_header(email_message['From'])[0]
+    if isinstance(from_header[0], bytes):
+        # if it's a bytes type, decode to str
+        from_header = from_header[0].decode(from_header[1])
+    senders.append(from_header)
+
+# unique senders
+unique_senders = list(set(senders))
+
+# print each sender
+for sender in unique_senders:
+    print(sender)
